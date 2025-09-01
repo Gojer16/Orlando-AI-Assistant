94eed738b8a207ae393f418dc07829af9af6a3c1
Gojer <gojer@naver.com>
2025-09-01 00:14:10 -0400

    Adding rate limit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from routes.chat import chat_bp
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 def create_app():
     """
@@ -9,7 +11,14 @@ def create_app():
     is a thin bootstrapper and routes live in routes/.
     """
     app = Flask(__name__)
-    CORS(app, origins=["http://localhost:5173"])
+    CORS(app, origins=["https://react-basic-resume.vercel.app/"]) # URL
+
+    limiter = Limiter(
+        key_func=get_remote_address,   # identifies clients by IP
+        app=app,
+        default_limits=["20 per minute"]  # global fallback
+    )
+
     app.register_blueprint(chat_bp) # attach chat API
 
     return app
